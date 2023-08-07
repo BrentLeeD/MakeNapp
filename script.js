037c98f132e47c11f6acba4051fa29758d19ca8d
BrentLeeD <140761233+BrentLeeD@users.noreply.github.com>
2023-08-07 09:21:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,20 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const totalCost = document.getElementById("totalCost");
 
   // Challah prices in ZAR
-  const chocolatePrice = 35;
+  const chocolatePrice = 40;
   const sesamePrice = 35;
   const plainPrice = 35;
   const streuselPrice = 40;
 
   // Week 1 to Week 6 prices
   const weekPrices = {
-    week1: 0,
     week2: 0,
     week3: 0,
     week4: 0,
     week5: 0,
     week6: 0,
   };
+
+  // Store selected flavors and quantities
+  let selectedFlavors = {};
 
   addToBasketBtn.addEventListener("click", function () {
     const selectedWeek = weekSelect.value;
@@ -54,6 +56,14 @@ document.addEventListener("DOMContentLoaded", function () {
       basketItem.textContent = `${getWeekDate(selectedWeek)}: ${orderSummary}`;
       basketContents.appendChild(basketItem);
 
+      // Store selected flavors and quantities in the global variable
+      selectedFlavors[selectedWeek] = {
+        chocolate: chocolateQuantity,
+        sesame: sesameQuantity,
+        plain: plainQuantity,
+        streusel: streuselQuantity,
+      };
+
       // Update weekPrices object
       weekPrices[selectedWeek] =
         chocolateQuantity * chocolatePrice +
@@ -78,6 +88,8 @@ document.addEventListener("DOMContentLoaded", function () {
       for (const week in weekPrices) {
         weekPrices[week] = 0;
       }
+      // Clear the selectedFlavors object
+      selectedFlavors = {};
       updateSubtotal();
     }
   });
@@ -87,25 +99,43 @@ document.addEventListener("DOMContentLoaded", function () {
       "That's a yummy order! We just need to confirm your details before you complete the order (payment will take place on a secure website)."
     );
     if (confirmation) {
-      const parentName = prompt("Please enter your full name:");
-      const phoneNumber = prompt("Please enter your phone number:");
-      const email = prompt("Please enter your email:");
+      const parentName = prompt("Please enter your full name and contact details:");
+      const phoneNumber = prompt("your child's name and class:");
 
-      if (parentName && phoneNumber && email) {
+      if (parentName && phoneNumber) {
         const orderDetails = `
           Parent's Name: ${parentName}
           Phone Number: ${phoneNumber}
-          Email: ${email}
-
+        
           Order Details:
           ${getOrderSummary()}
         `;
-
         alert(orderDetails);
-
+        
         const totalAmount = calculateTotalAmount();
-        const checkoutUrl = `https://pos.snapscan.io/qr/Bu-elYzb?id=challah_${parentName}&amount=${totalAmount}`;
+const orderData = {
+          Parent: parentName,
+	  Child: phoneNumber,
+          'Order Breakdown': orderDetails,
+          Cost: `${totalAmount} ZAR`,
+        };
+        const apiKey = 'UmaiF3c39grmhByWn4MPZVYfklm90CFWPZxRx8cfi3MZrYXEt98tK4Tp7wo';
+        const sheetId = '1A2c5YPjGiMzbTco8MsCbNCHDimWycyrq1iVd1Pulyjg';
+        const apiUrl = `https://api.sheetson.com/v2/sheets/CHALLAH`;
+
+        fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            'X-Spreadsheet-Id': sheetId,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(orderData),
+        })
+                const checkoutUrl = `https://pos.snapscan.io/qr/Bu-elYzb?id=challah_${parentName}&amount=${totalAmount}00`;
         window.open(checkoutUrl, "_blank");
+        // Clear the form and the selected flavors and quantities after checkout
+        clearForm();
       } else {
         alert("Please fill out all the required information.");
       }
@@ -116,10 +146,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const orderSummary = [];
     for (const week in weekPrices) {
       if (weekPrices[week] > 0) {
-        orderSummary.push(`${getWeekDate(week)}: R${weekPrices[week]}`);
+        orderSummary.push(`${getWeekDate(week)}:\n${getOrderItems(week)}\nTotal Cost: R${weekPrices[week]}`);
       }
     }
-    return orderSummary.join("\n");
+    return orderSummary.join("\n\n");
+  }
+
+  function getOrderItems(week) {
+    const orderItems = [];
+    const flavors = selectedFlavors[week];
+
+    if (flavors.chocolate > 0) {
+      orderItems.push(`${flavors.chocolate} x Chocolate`);
+    }
+    if (flavors.sesame > 0) {
+      orderItems.push(`${flavors.sesame} x Sesame`);
+    }
+    if (flavors.plain > 0) {
+      orderItems.push(`${flavors.plain} x Plain`);
+    }
+    if (flavors.streusel > 0) {
+      orderItems.push(`${flavors.streusel} x Streusel`);
+    }
+
+    return orderItems.join("\n");
   }
 
   function calculateTotalAmount() {
@@ -132,8 +182,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function getWeekDate(week) {
     switch (week) {
-      case "week1":
-        return "3 August";
       case "week2":
         return "10 August";
       case "week3":
@@ -156,5 +204,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     totalCost.textContent = `Total Cost: ${overallTotal} ZAR`;
+  }
+
+  function clearForm() {
+    chocolateInput.value = "0";
+    sesameInput.value = "0";
+    plainInput.value = "0";
+    streuselInput.value = "0";
+
+    // Clear the selectedFlavors object
+    selectedFlavors = {};
+
+    // Clear the basketContents
+    basketContents.innerHTML = "";
+
+    // Reset weekPrices
+    for (const week in weekPrices) {
+      weekPrices[week] = 0;
+    }
+
+    // Update the total cost
+    updateSubtotal();
   }
 });
